4a092453b81662c1f4e9c5ee13f8d032dd38a87d
mdaminuddinshah <muhammadaminuddinshah98@gmail.com>
2024-04-01 12:04:42 +0800

    add sql notes

---
+++ sql/index.sql
@@ -0,0 +1,13 @@
+-- HOW TO SET UP A DATABASE AND POSTGRES
+-- open tableplus to connect with postgres server
+-- default password is postgres
+-- default port is 5432
+-- name can be anything, 
+-- leave database empty for moment
+
+-- after that, connect. 
+-- inside a postgress server can create many database,
+-- so create database with new
+-- close tableplus, and open again search for database you create and connect with same password and port
+
+-- 
